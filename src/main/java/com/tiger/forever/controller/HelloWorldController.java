d54bfc9ae610008572fd9e2cc537da0bfcc6c131
dongxuehu <dongxuehu@megalinkware.com>
2023-06-10 17:45:58 +0800

    测试回调

--- src/main/java/com/tiger/forever/controller/HelloWorldController.java
+++ src/main/java/com/tiger/forever/controller/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.tiger.forever.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     public  String hello(){
         System.out.println("--------------4125");
         return "hello world2";
