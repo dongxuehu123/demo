8132cd46893533b599dcb49198e169c928e7f8ac
dongxuehu <dongxuehu@megalinkware.com>
2023-06-10 10:23:05 +0800

    初始化

--- src/main/java/com/tiger/forever/controller/HelloWorldController.java
+++ src/main/java/com/tiger/forever/controller/HelloWorldController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public  String hello(){
+        System.out.println("--------------");
         return "hello";
     }
 }
