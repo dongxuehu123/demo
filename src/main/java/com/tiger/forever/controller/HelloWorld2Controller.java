1492e3a80cbdd034b2d142b3cc674d38c0016462
dongxuehu <dongxuehu@megalinkware.com>
2023-06-10 17:36:16 +0800

    测试回调

---
+++ src/main/java/com/tiger/forever/controller/HelloWorld2Controller.java
@@ -0,0 +1,15 @@
+package com.tiger.forever.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("apitest")
+public class HelloWorld2Controller {
+    @GetMapping("/hello")
+    public  String hello(){
+        System.out.println("--------------4125");
+        return "hello world2";
+    }
+}
